5da379587b1447cf0c4b8ea1145f5ade1dbd2949
jamesc2000 <cruz.james99@gmail.com>
2020-02-23 10:47:31 +0800

    bare js for capacity and quantity

--- order_product/subtotal.js
+++ order_product/subtotal.js
@@ -1,8 +1,22 @@
 var subtotal_value = document.getElementById("subtotal_value");
+var capacity;
 
 function update()
 {
+    var eight = document.getElementById("8g");
+    var sixteen = document.getElementById("16g");
+    var thirtytwo = document.getElementById("32g");
+
     var quantity = document.getElementById("quantity").value;
-    var price = quantity * 10;
+    if (eight.checked)
+    {
+        capacity = 180;
+    } else if (sixteen.checked) {
+        capacity = 249;
+    } else if (thirtytwo.checked) {
+        capacity = 429;
+    }
+
+    var price = quantity * capacity;
     subtotal_value.innerHTML = price;
 }
